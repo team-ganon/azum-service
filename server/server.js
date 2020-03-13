c49d2cd102968dcc4b59df3bc30cb508466d9099
Azum Beg <azumbeg@gmail.com>
2020-03-12 21:44:06 -0500

    Connect client and server

--- server/server.js
+++ server/server.js
@@ -3,13 +3,25 @@ const path = require("path");
 const mongoose = require("mongoose");
 mongoose.Promise = require("bluebird");
 
-const Property = require("../db/schema.js");
+const Rental = require("./db/Rental.js");
 
-mongoose.connect("http://localhost:27017", { useMongoClient: true });
+mongoose.connect("mongodb://localhost:27017/airbnb", { useNewUrlParser: true, useUnifiedTopology: true });
 
 var app = express();
 
 app.use(express.static(path.resolve(__dirname, "../public")));
+
+app.get("/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  console.log(id);
+  Rental.findOne({ _id: id })
+    .then(result => {
+      res.json(result);
+    })
+    .catch(err => {
+      res.status(400).send(err);
+    });
+});
 
 app.listen(3003, err => {
   console.log("Listening on port 3003...");
