7405a170888ec2e9ff7ab6b66fb5eece5f212379
Azum Beg <azumbeg@gmail.com>
2020-03-25 13:39:21 -0500

    Set up GuestBar component

---
+++ client/components/GuestBar/GuestBar.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import styles from "./GuestBar.css";
+
+class GuestBar extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    <div>Guest Bar</div>
+  }
+}
+
+export default GuestBar;
